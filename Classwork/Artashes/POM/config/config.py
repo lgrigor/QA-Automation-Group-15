da8930b83c053deec07637372308c61f8dc0b89c
lgrigor <87751350+lgrigor@users.noreply.github.com>
2022-05-21 11:39:25 +0400

    Delete Classwork/Artashes directory

--- Classwork/Artashes/POM/config/config.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class TestData():
-    url_Textbox = "https://demoqa.com/text-box"
-    url_Checkbox = "https://demoqa.com/checkbox"
-    url_Radio = "https://demoqa.com/radio-button"
-    anun = "Artashes"
-   
